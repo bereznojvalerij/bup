823e7715b3784d8cfd125efd6303e07bf612abd4
Валерий <valerij@MacBook-Air-Valerij.local>
2026-04-22 20:23:39 +0300

    second stable version

--- handlers/fart.py
+++ handlers/fart.py
@@ -1,45 +1,40 @@
 from aiogram import Router
-from aiogram.types import Message
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 
-from database import get_user_id, add_event, get_all_users
-from config import MAX_FART_PER_INPUT
+from database import get_user_id, add_event
 from keyboards.main import fart_keyboard, main_keyboard
 
 router = Router()
 
 
-class FartState(StatesGroup):
-    waiting_for_count = State()
-
-
 @router.callback_query(lambda c: c.data == "fart")
 async def fart_menu(callback: CallbackQuery):
+    await callback.answer()
     await callback.message.edit_text(
         "Выбери количество:",
         reply_markup=fart_keyboard()
     )
 
+
 @router.callback_query(lambda c: c.data.startswith("fart_"))
 async def add_fart(callback: CallbackQuery):
+    await callback.answer()
     count = int(callback.data.split("_")[1])
 
     user_id = await get_user_id(callback.from_user.id)
     await add_event(user_id, count)
 
-    text = f"💨 {callback.from_user.first_name} сделал {count} пук(а)!"
-
-    await callback.message.answer(text, reply_markup=main_keyboard())
+    text = f"💨 {callback.from_user.first_name} сделал {count}!"
 
     await callback.message.edit_text(
-        "Меню:",
+        text,
         reply_markup=main_keyboard()
     )
 
+
 @router.callback_query(lambda c: c.data == "back")
 async def back(callback: CallbackQuery):
+    await callback.answer()
     await callback.message.edit_text(
         "Меню:",
         reply_markup=main_keyboard()
